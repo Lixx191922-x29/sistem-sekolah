a07cb5cb6b77d1dd93d1d969f2354a3e0a131a97
Lixx191922-x29 <felixfel0884@gmail.com>
2026-02-03 07:41:42 +0700

    feat: implement router app

--- public/index.php
+++ public/index.php
@@ -4,5 +4,12 @@ use App\Core\Router;
 
 $router = new Router();
 
+
+// Register Routers
+$router->add('GET','/students', 'StudentController', 'index');
+$router->add('GET','/students/create', 'StudentController', 'create');
+$router->add('GET','/students/{id}', 'StudentController', 'show');
+
 $router->run();
+
 ?>
