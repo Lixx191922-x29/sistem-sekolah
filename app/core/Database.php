9e18b2e4c407e75abfee68294c6f323aa24e5f95
Lixx191922-x29 <felixfel0884@gmail.com>
2026-04-14 07:47:41 +0700

    feat: implement database layer

---
+++ app/core/Database.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Core;
+require_once '../app/config/app.php';
+
+class Database
+{
+
+    protected function_construct()
+    {
+        $this->connection = mysqli_connect(
+            DB_HOST,
+            DB_USER,
+            DB_PASSWORD,
+            DB_NAME
+        );
+
+        if (!$this->connection) {
+            die('ERROR TO CONNECT DATABASE');
+        }
+    }
+
+}
+
+
+?>
