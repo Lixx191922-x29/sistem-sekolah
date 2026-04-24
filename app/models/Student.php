a4ecf5e6ba18e3519a9aea1ec764b783f8a4d69c
Lixx191922-x29 <felixfel0884@gmail.com>
2026-04-24 11:42:51 +0700

    feat: implement edit dan delete student

--- app/models/Student.php
+++ app/models/Student.php
@@ -42,7 +42,7 @@ class Student extends Database
         }
 
         //Menambahkan Data Siswa
-        public function inset(array $data)
+        public function insert(array $data)
         {
             $name = htmlspecialchars($data['name']);
             $nis = htmlspecialchars($data['nis']);
@@ -60,6 +60,46 @@ class Student extends Database
                 exit();
             } else {
                 echo 'ERROR TO STORE STUDENT';
+            }
+        }
+
+        //Mengupdet data siswa
+        public function update(array $data, int $id)
+        {
+            $name = htmlspecialchars($data['name']);
+            $nis = htmlspecialchars($data['nis']);
+            $class = htmlspecialchars($data['class']);
+            $phoneNumber = htmlspecialchars($data['phone_number']);
+
+            $query = "UPDATE {$this->table} SET name = ?, nis = ?, class = ?, phone_number = ? WHERE id = ?";
+
+            $stmt = $this->connection->prepare($query);
+            $stmt->bind_param('ssssi', $name, $nis, $class, $phoneNumber, $id );
+            $stmt->execute();
+
+            if($stmt->affected_rows > 0) {
+                header('Location: /students');
+                exit();
+            } else {
+                echo 'ERROR TO UPDATE STUDENT';
+            }
+        }
+
+        //Menghapus data siswa
+         public function delete(int $id)
+        {
+
+            $query = "DELETE FROM {$this->table} WHERE id = ?";
+
+            $stmt = $this->connection->prepare($query);
+            $stmt->bind_param('i', $id);
+            $stmt->execute();
+
+            if($stmt->affected_rows > 0) {
+                header('Location: /students');
+                exit();
+            } else {
+                echo 'ERROR TO DELETE STUDENT';
             }
         }
 
